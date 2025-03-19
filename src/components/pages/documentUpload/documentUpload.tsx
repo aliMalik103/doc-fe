54fbfda63e381fbcd1a39a6c7c114c7883584285
Ali Hasnain <ayshaareej71@gmail.com>
2025-03-19 18:54:09 +0500

    height fix

--- src/components/pages/documentUpload/documentUpload.tsx
+++ src/components/pages/documentUpload/documentUpload.tsx
@@ -79,7 +79,7 @@ const DocumentUpload: React.FC = () => {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', height: '100vh', background: '#f4f4f9', padding: '20px', width: '100vw' }}>
-      <div style={{ background: 'white', padding: '30px', borderRadius: '10px', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)', width: '80%', maxWidth: '600px', textAlign: 'center', marginBottom: '20px' }}>
+      <div style={{ background: 'white', padding: '30px', borderRadius: '10px', height:"210px",boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)', width: '80%', maxWidth: '600px', textAlign: 'center', marginBottom: '20px' }}>
         <h2 style={{ marginBottom: '20px' }}>Upload Document</h2>
         <div>
           <input
